395174017543bfc29c8664dbc1aa605ce9086875
OsandaLelum <41205668+OsandaLelum@users.noreply.github.com>
2019-07-20 20:21:08 +0530

    Add files via upload

---
+++ Osa2.scala
@@ -0,0 +1,18 @@
+object Osa2{
+    def main(args :Array[String]){
+        val name = "Oasnda Lelum"
+        val age =18.5
+        println(name+ " is " +age+ " years old")
+          println("S string interpolution")
+        println(s"$name is $age  years old")
+
+             println("f string interpolution")
+             //  string is  not type safe
+         println(f"$name%s  is  $age%f years old")
+
+         println("Hello \n World")
+         println(s"Hello \n Osanda")
+         println(raw"Hello \n world")
+
+         }
+}
---
+++ osa.scala
@@ -0,0 +1,38 @@
+object  osa{
+    def main(args:Array[String]){
+            println("Hello osa")
+
+             //the immutable and the mutable. The difference between mutable and immutable objects is that when an object is immutable, the object itself can't be changed
+             //scala  normally defined imutable Map
+             // if  we want to use mutable   we should import the   ->scala  collection.mutable.Map
+             
+             // if we want to usxe mutable and immutable map in the same object 
+             //then  refer the mutable map as mutable.Map
+
+
+             //Empty hash table whose keys are strings and values are integers 
+            
+
+             var colors =Map("red"->"#FF0000","azure"->"F)FFF")
+            println(colors)
+
+         var capital =Map("US"->"washington","France"->"Paris","SL"->"Sri JAyawardanepurar ")
+            println(capital)
+
+            capital +=("japan "->"tokyo")
+
+           // println(capital(US))
+            println(capital);
+
+             var Names =Map("1"->"osaa","2"->"yasha","3"->"visu","4"->"anu")
+             Names +=("1"->"gami","6"->"chooty","7"->"sudu")*/
+             println(Names)
+
+
+             A +=("I"->"1","J"->"5")
+
+
+             println(A)
+
+    }
+}
