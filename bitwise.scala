080dca671f4b69b96fd836dcd2e8646930d949cf
OsandaLelum <41205668+OsandaLelum@users.noreply.github.com>
2019-07-25 17:36:42 +0530

    Add files via upload

---
+++ bitwise.scala
@@ -0,0 +1,21 @@
+object bitwise{
+	def main(args:Array[String]){
+
+		val a=90
+		val b=100
+
+		println(a&b)
+
+		println(a&b)
+		println(a|b)
+		println(a^b)
+		println(a<<2)
+		println(a>>2)
+		println(a>>>3)
+		
+         //shift right zero fill operator
+		//println(a<<<2)
+	
+
+	}
+}
